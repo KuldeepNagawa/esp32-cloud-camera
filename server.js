87737e54e00b648f27c0a33f9284954df26b4fc8
KuldeepNagawa <kuldeepnagawa7@gmail.com>
2026-02-17 14:27:07 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+app.use(express.raw({ type: "image/jpeg", limit: "10mb" }));
+
+const FRAME_PATH = path.join(__dirname, "frame.jpg");
+
+app.post("/upload", (req, res) => {
+  fs.writeFileSync(FRAME_PATH, req.body);
+  res.send("Frame received");
+});
+
+app.get("/live", (req, res) => {
+  if (fs.existsSync(FRAME_PATH)) {
+    res.setHeader("Content-Type", "image/jpeg");
+    res.sendFile(FRAME_PATH);
+  } else {
+    res.send("No frame yet");
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
